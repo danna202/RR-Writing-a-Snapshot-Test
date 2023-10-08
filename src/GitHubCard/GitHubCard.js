773597f540109220f6d6027280447f4a538dddc4
danna202 <105758399+danna202@users.noreply.github.com>
2023-10-07 22:27:56 -0400

    Update GitHubCard.js

--- src/GitHubCard/GitHubCard.js
+++ src/GitHubCard/GitHubCard.js
@@ -4,19 +4,28 @@ import MickeyMouse from './Mickey_Mouse.png'
 
 function GitHubCard() {
   return (
-      <div className="card">
-        <Card style={{ width: "18rem"}}>
-        {/* Use your GitHub info instead, but Mickey Mouse is used as an example */}
-        <Card.Img variant="top" src={MickeyMouse} />
-        {/* Source: https://en.wikipedia.org/wiki/Mickey_Mouse */}
-        <Card.Body>
-            <Card.Title>Mickey Mouse</Card.Title>
-            <Card.Text>
-            I am a cartoon character and am one of the world's most recognizable fictional characters.
-            </Card.Text>
-        </Card.Body>
-        </Card>
-      </div>
+    <Card  style="width: 18rem;">
+      <Card.Img variant="top" src={MickeyMouse} />
+      <Card.Body>
+        <Card.Title>Mickey Mouse</Card.Title>
+        <Card.Text>
+          I am a cartoon character and am one of the world's most recognizable fictional characters.
+        </Card.Text>
+      </Card.Body>
+    //   <div className="card">
+    //     <Card style={{ width: "18rem"}
+    //     >
+        
+    //     <Card.Img variant="top" src={MickeyMouse} />
+ 
+    //     <Card.Body>
+    //         <Card.Title>Mickey Mouse</Card.Title>
+    //         <Card.Text>
+    //         I am a cartoon character and am one of the world's most recognizable fictional characters.
+    //         </Card.Text>
+    //     </Card.Body>
+    //     </Card>
+    //   </div>
   );
 }
 
